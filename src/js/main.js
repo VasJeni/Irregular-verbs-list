572d95e19fc67a9e43991b0663541285a0d67e24
Vasjeni <vasilyev508@gmail.com>
2022-09-07 17:27:15 +0100

    rewrite translate of some words

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,41 @@
 "use strict"
-const WORDS_IN_PART = 1
+const WORDS_IN_PART = 2
 const TABLE_HEAD = ['#', 'base form', 'past simple', 'past participle']
-const BASE_FORM = ["beat", "become", "begin", "bend", "bet", "bite", "bleed", "blow", "break", "breed", "bring",
+const BASE_FORM = ["be","beat", "become", "begin", "bend", "bet", "bite", "bleed", "blow", "break", "breed", "bring",
     "build", "burn", "buy", "catch", "choose", "come", "cost", "cut", "do", "dig", "draw", "dream", "drink", "drive",
     "eat", "fall", "feed", "feel", "fight", "find", "fly", "forget", "forgive", "freeze", "get", "give", "go", "grow",
-    "have", "hear", "hide", "hit", "hold", "hurt", "keep", "know", "lay", "lead", "lean", "leave", "lend", "let",
+    "hang","have", "hear", "hide", "hit", "hold", "hurt", "keep", "know", "lay", "lead", 'learn', "lean", "leave", "lend", "let",
     "lose", "make", "mean", "meet", "pay", "put", "quit", "read", "ride", "ring", "rise", "run", "say",
     "see", "sell", "send", "set", "shake", "shine", "shoe", "shoot", "show", "shrink", "shut", "sing", "sink", "sit",
-    "sleep", "speak", "spend", "spill", "spread", "speed", "stand", "steal", "stick", "sting", "stink", "swear",
+    "sleep", 'smell',"speak", "spend", "spill", "spread", "speed", "stand", "steal", "stick", "sting", "stink", "swear",
     "sweep", "swim", "swing", "take", "teach", "tear", "tell", "think", "throw", "understand", "wake", "wear", "win",
     "write"]
-const PAST_SIMPLE = ["beat", "became", "began", "bent", "bet", "bit", "bled", "blew", "broke", "bred", "brought", "built",
+const PAST_SIMPLE = ['was/were',"beat", "became", "began", "bent", "bet", "bit", "bled", "blew", "broke", "bred", "brought", "built",
     "burnt/burned", "bought", "caught", "chose", "came", "cost", "cut", "did", "dug", "drew", "dreamt/dreamed", "drank", "drove",
-    "ate", "fell", "fed", "felt", "fought", "found", "flew", "forgot", "forgave", "froze", "got", "gave", "went", "grew", "had",
-    "heard", "hid", "hit", "held", "hurt", "kept", "knew", "laid", "led", "leant/leaned", "left", "lent", "let", "lost", "made",
+    "ate", "fell", "fed", "felt", "fought", "found", "flew", "forgot", "forgave", "froze", "got", "gave", "went", "grew","hung", "had",
+    "heard", "hid", "hit", "held", "hurt", "kept", "knew", "laid", "led",'learned/learnt', "leant/leaned", "left", "lent", "let", "lost", "made",
     "meant", "met", "paid", "put", "quit", "read", "rode", "rang", "rose", "ran", "said", "saw", "sold", "sent", "set",
-    "shook", "shone", "shod", "shot", "showed", "shrank", "shut", "sang", "sank", "sat", "slept", "spoke", "spent", "spilt/spilled",
+    "shook", "shone", "shod", "shot", "showed", "shrank", "shut", "sang", "sank", "sat", "slept", "smelled/smelt", "spoke", "spent", "spilt/spilled",
     "spread", "sped", "stood", "stole", "stuck", "stung", "stank", "swore", "swept", "swam", "swung", "took", "taught", "tore",
     "told", "thought", "threw", "understood", "woke", "wore", "won", "wrote"]
-const PAST_PARTICIPLE = ["beaten", "become", "begun", "bent", "bet", "bitten", "bled", "blown", "broken", "bred", "brought",
+const PAST_PARTICIPLE = ["been", "beaten", "become", "begun", "bent", "bet", "bitten", "bled", "blown", "broken", "bred", "brought",
     "built", "burnt/burned", "bought", "caught", "chosen", "come", "cost", "cut", "done", "dug", "drawn", "dreamt/dreamed",
     "drunk", "driven", "eaten", "fallen", "fed", "felt", "fought", "found", "flown", "forgotten", "forgiven", "frozen", "got",
-    "given", "gone", "grown", "had", "heard", "hidden", "hit", "held", "hurt", "kept", "known", "laid", "led", "leant/leaned", "left",
+    "given", "gone", "grown",'hung', "had", "heard", "hidden", "hit", "held", "hurt", "kept", "known", "laid", "led" ,'learned/learnt', "leant/leaned", "left",
     "lent", "let", "lost", "made", "meant", "met", "paid", "put", "quit", "read", "ridden", "rung", "risen", "run", "said",
-    "seen", "sold", "sent", "set", "shaken", "shone", "shod", "shot", "shown", "shrunk", "shut", "sung", "sunk", "sat", "slept",
+    "seen", "sold", "sent", "set", "shaken", "shone", "shod", "shot", "shown", "shrunk", "shut", "sung", "sunk", "sat", "slept", 'smelled/smelt',
     "spoken", "spent", "spilt/spilled", "spread", "sped", "stood", "stolen", "stuck", "stung", "stunk", "sworn", "swept", "swum",
     "swung", "taken", "taught", "torn", "told", "thought", "thrown", "understood", "woken", "worn", "won", "written"]
-const TRANSLATE = ["бити", "стати", "почати", "зігнути", "ставка", "укус", "кровоточити", "удар", "перерву", "порода",
-    "принести", "будувати", "спалювати", "купити", "зловити/упіймати", "вибрати", "прийти", "вартість", "вирізати", "робити", "копати",
-    "малювати", "сон", "пити", "диск", "їсти", "падіння", "годувати", "відчувати", "боротися", "знайти", "літати", "забути",
-    "пробачити", "замерзнути", "отримати", "дати", "йти", "рости", "мати", "чути", "приховати", "удар", "утримувати", "боляче",
-    "зберегти", "знати", "лежати", "вести", "худий", "залишати", "позичати", "дозволяє", "втрачати", "зробити", "означає",
-    "зустріти", "платити", "поставити", "кинути", "прочитати", "їздити", "кільце", "підніматися", "бігати", "сказати",
-    "побачити", "продавати", "відправити", "встановити", "струсити", "блиск", "взуття", "стріляти", "показувати", "скорочуватися",
-    "закрити", "співати", "раковина", "сидіти", "сон", "говорити", "витрачати", "розлив", "поширення", "швидкість", "стояти",
-    "красти", "палка", "жало", "сморід", "лаятись", "підмітати", "плавати", "гойдалка", "брати", "навчати", "рвати",
-    "розповідати", "думаю", "кинути", "зрозуміти", "розбудити", "знос", "виграти", "писати"]
+const TRANSLATE = ["бути","бити", "стати", "починати", "зігнути", "ставка", "кусати", "кровоточити", "дути/дмухати", "ломати", "порода",
+    "принести", "будувати", "спалювати", "купувати", "упіймати", "вибирати", "прийти", "вартість", "різати", "робити", "копати",
+    "малювати", "мріяти", "пити", "керувати(автомобілем)", "їсти", "падати", "годувати", "відчувати", "боротися", "шукати", "літати", "забувати",
+    "пробачити", "замерзнути", "отримати", "давати", "йти", "рости", 'вішати',"мати", "чути", "сховати", "ударяти/бити", "утримувати", "робити боляче",
+    "зберігати/утримувати", "знати", "лежати", "вести",'вчитись', "худий", "залишати", "позичати", "дозволяти", "втрачати", "зробити", "означає",
+    "зустрічати", "платити", "ставити", "кинути", "читати", "їздити(верхи)", "дзвонити", "підніматися", "бігати", "казати",
+    "бачити", "продавати", "відправити", "встановити", "струсити", "блищати, (светить)", "взуття", "стріляти", "показувати", "скорочуватися",
+    "закрваити", "співати", "раковина", "сидіти", "спати", 'пахнути', "говорити", "витрачати", "розлив", "поширення", "швидкість", "стояти",
+    "красти", "застрягати", "жало", "сморід", "лаятись", "підмітати", "плавати", "гойдатись", "брати", "навчати", "рвати",
+    "розповідати", "думати", "кинути", "зрозуміти", "прокидатись/будити", "носити одяг", "виграти", "писати"]
 const DICTIONARY_LIST = [TRANSLATE, BASE_FORM, PAST_SIMPLE, PAST_PARTICIPLE]
 const NAMES_BUTTONS = ['learn', 'exam', 'test', 'check']
 const BUTTONS = createButtons(NAMES_BUTTONS)
